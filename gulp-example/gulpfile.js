4066446fd163802d7f77028081a92cbfd1fc96ad
nikonst <nikonst@otenet.gr>
2020-12-28 12:44:09 +0200

    Use gulp default (series)

--- gulp-example/gulpfile.js
+++ gulp-example/gulpfile.js
@@ -3,29 +3,39 @@ const imagemin = require('gulp-imagemin')
 const minify = require('gulp-minify');
 const cleanCSS = require('gulp-clean-css');
 
+//Copy data file
+gulp.task("copyData", async () => {
+    gulp.src('src/data/*.json')
+        .pipe(gulp.dest('dist/data'))
+})
+
 //Copy HTML files
 gulp.task("copyHTML", async () => {
     gulp.src('src/*.html')
-    .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist'))
 })
 
 //Optimize images
 gulp.task("imageMin", async () => {
     gulp.src("src/imgs/*.png")
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/imgs'))
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/imgs'))
 })
 
 //Minify js
 gulp.task("minifyJS", async () => {
     gulp.src("src/js/*.js")
-    .pipe(minify())
-    .pipe(gulp.dest('dist/js'))
+        .pipe(minify())
+        .pipe(gulp.dest('dist/js'))
 })
 
 //Minify css
 gulp.task("minifyCSS", async () => {
     gulp.src("src/css/*.css")
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('dist/css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist/css'))
 })
+
+//Run all tasks
+gulp.task('default', gulp.series('copyData', 'copyHTML', 'imageMin', 'minifyJS', 'minifyCSS'))
+
