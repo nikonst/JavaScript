f6267a11da2e7a3d09b39c9437e8987d215aebbb
nikonst <nikonst@otenet.gr>
2020-12-28 11:47:35 +0200

    Add gulp imagemin

--- gulp-example/gulpfile.js
+++ gulp-example/gulpfile.js
@@ -1,2 +1,15 @@
-import gulp from 'gulp';
+const gulp = require('gulp')
+const imagemin = require('gulp-imagemin');
 
+//Copy HTML files
+gulp.task("copyHTML", async () => {
+    gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'))
+})
+
+//Optimize images
+gulp.task("imageMin", async () => {
+    gulp.src("src/imgs/*.png")
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/imgs'))
+})
