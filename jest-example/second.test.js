d81b1bf4a0371d97f188949fa3f74c14be70d54e
nikonst <nikonst@otenet.gr>
2020-12-30 11:41:10 +0200

    Add mocking

--- jest-example/second.test.js
+++ jest-example/second.test.js
@@ -1,6 +1,6 @@
 import users from './data/data.json'
 
-test('the data are coreect', () => {
+test('the data are corect', () => {
     expect(users).toMatchSnapshot()
     expect(users).toHaveLength(28)
 })
@@ -13,5 +13,49 @@ users.forEach(element => {
         expect(element).toHaveProperty('email')
         expect(element).toHaveProperty('country')
     })
-    
-});
+})
+
+test('Spying on function original implementation...', () => {
+    const user = {
+        name: str => `User name ${str}`
+    }
+    const spy = jest.spyOn(user, 'name')
+    expect(user.name('Taylor')).toBe('User name Taylor')
+    expect(spy).toHaveBeenCalledWith('Taylor')
+})
+
+test('Spying on function mock implementation...', () => {
+    const user = {
+        name: str => `User name ${str}`
+    }
+    const spy = jest.spyOn(user, 'name')
+    spy.mockImplementation(str => `Hello ${str}`)
+    expect(user.name('Taylor')).toBe('Hello Taylor')
+    spy.mockRestore()
+    expect(user.name('Taylor')).toBe('User name Taylor')
+})
+
+test('Match data', () => {
+    const fourthUser = {
+        "id": "16300714-1769",
+        "name": "Zenia",
+        "surname": "Sampson",
+        "email": "consectetuer@incursus.edu",
+        "country": "Guinea-Bissau"
+    }
+    expect(users[3]).toMatchObject(fourthUser)
+})
+
+test('Expect a Promise to resolve', async () => {
+    const user = {
+        getName: jest.fn(() => Promise.resolve('John Smith'))
+    }
+    await expect(user.getName()).resolves.toBe('John Smith')
+})
+
+test('Expect a Promise to reject', async () => {
+    const user = {
+        getName: jest.fn(() => Promise.reject(new Error('An error occured')))
+    }
+    await expect(user.getName()).rejects.toThrow('An error occured')
+})
