aa5aaccfa6901107f3a54cf9fca58e4cd6a878ab
nikonst <nikonst@otenet.gr>
2020-12-30 23:39:21 +0200

    Add static method

--- ES6Classes/index.js
+++ ES6Classes/index.js
@@ -8,10 +8,18 @@ class Car {
     carStats() {
         return `This car has ${this.doors} doors, ${this.engine} engine and ${this.color} color`
     }
+
+    static totalDoors(c1, c2) {
+        const doors1 = c1.doors
+        const doors2 = c2.doors
+        return doors1 + doors2
+    }
 }
 
-const csx5 =  new Car(5, 'v6', 'blue')
+const csx5 = new Car(5, 'v6', 'blue')
+const suv1 = new Car(5, 'v8', 'red')
 
 console.log(csx5)
 console.log(csx5.doors)
 console.log(csx5.carStats())
+console.log(Car.totalDoors(csx5, suv1))
