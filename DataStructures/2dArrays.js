d66789fcc98cec750e9a95343ba2aefe6db46fca
nikonst <nikonst@otenet.gr>
2021-01-04 23:01:19 +0200

    2d Arrays

---
+++ DataStructures/2dArrays.js
@@ -0,0 +1,63 @@
+// JavaScript 2D Arrays
+
+// A multidimensional array is an array that contains another array
+let data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+// Access elements
+console.log(data)
+console.log(data[0])
+console.log(data[1])
+console.log(data[0][0])
+console.log(data[1][2])
+
+// Adding Element to the Outer Array
+data.push([10, 11, 12])
+console.log(data)
+
+// Adding Element to the Inner Array
+data[1].push(6.5)
+console.log(data)
+
+// Remove Element from Outer Array
+
+data.pop()
+console.log(data)
+
+// Remove Element from Inner Array
+data[1].pop()
+console.log(data)
+
+// Using splice
+data.splice(0, 1)
+console.log(data)
+
+data[1].splice(0, 1)
+console.log(data)
+
+// Iterating
+console.group('----------------------------')
+let data2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+console.log(data2)
+
+// Iterate I
+for (i of data2) {
+    console.log(i)
+}
+
+for (i of data2)
+    for (j of i)
+        console.log(j)
+
+// Iterate II
+data2.forEach(element => {
+    console.log(element)
+})
+
+console.log("++++++++++++++++++")
+data2.forEach(element => {
+    element.forEach(el => {
+        console.log(el)
+    })
+})
+
+// High oredr functions on 2d arrays
