4c9a01e767cfc155dafea5bd506f59877535e1af
nikonst <nikonst@otenet.gr>
2021-01-04 22:59:16 +0200

    Put data in file

--- DataStructures/arrays.js
+++ DataStructures/arrays.js
@@ -1,3 +1,6 @@
+//const arrayOfObjects = require('./helpers/arrayData')
+const arrayOfObjects = require('./helpers/arrayData')
+
 const getRandomInt = require("./helpers/getRandomInt")
 
 var a = []
@@ -113,77 +116,7 @@ findGreaterThan30Index = a.findIndex(el => {
 console.log(findGreaterThan30Index)
 
 //ARRAY OF OBJECTS
-const aObjects = [{
-    "id": 1,
-    "first_name": "Wake",
-    "last_name": "Oldacres",
-    "email": "woldacres0@amazon.co.uk",
-    "gender": "Male",
-    "height": 191
-}, {
-    "id": 2,
-    "first_name": "Lisabeth",
-    "last_name": "Vlasenko",
-    "email": "lvlasenko1@nih.gov",
-    "gender": "Female",
-    "height": 214
-}, {
-    "id": 3,
-    "first_name": "Mora",
-    "last_name": "Roaf",
-    "email": "mroaf2@economist.com",
-    "gender": "Female",
-    "height": 175
-}, {
-    "id": 4,
-    "first_name": "Norina",
-    "last_name": "Beardsell",
-    "email": "nbeardsell3@blogtalkradio.com",
-    "gender": "Female",
-    "height": 184
-}, {
-    "id": 5,
-    "first_name": "Waly",
-    "last_name": "Sheddan",
-    "email": "wsheddan4@hhs.gov",
-    "gender": "Female",
-    "height": 176
-}, {
-    "id": 6,
-    "first_name": "Ralf",
-    "last_name": "Wittman",
-    "email": "rwittman5@yale.edu",
-    "gender": "Male",
-    "height": 175
-}, {
-    "id": 7,
-    "first_name": "Joe",
-    "last_name": "Andrejs",
-    "email": "jandrejs6@webs.com",
-    "gender": "Male",
-    "height": 184
-}, {
-    "id": 8,
-    "first_name": "Pancho",
-    "last_name": "Odam",
-    "email": "podam7@mac.com",
-    "gender": "Male",
-    "height": 182
-}, {
-    "id": 9,
-    "first_name": "Peterus",
-    "last_name": "Keach",
-    "email": "pkeach8@wordpress.com",
-    "gender": "Male",
-    "height": 218
-}, {
-    "id": 10,
-    "first_name": "Melesa",
-    "last_name": "Want",
-    "email": "mwant9@fda.gov",
-    "gender": "Female",
-    "height": 198
-}]
+const aObjects = arrayOfObjects
 
 sumHeights = aObjects.reduce((s, item) => {
     return s += item.height
