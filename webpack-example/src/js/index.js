e23a5ba62e525e7898bc226ca8c76c3e17940a88
Nikos Konstantakis <nikonst@sch.gr>
2021-01-27 10:34:08 +0200

    Adding babel and react

--- webpack-example/src/js/index.js
+++ webpack-example/src/js/index.js
@@ -3,5 +3,15 @@ const people = require('./../data/people.js')
 const $ = require('jquery')
 
 $.each(people, (key, value) => {
-    $('body').append('<h1>'+value.name+'</h1>')
+    $('body').append('<h1>' + value.name + '</h1>')
 })
+
+import React from 'react'
+import { render } from 'react-dom'
+
+const Greeting = () => <h2>Hello from React</h2>
+
+render(
+    <Greeting/>,
+    document.getElementById('react-target')   
+)
--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -20,6 +20,17 @@ module.exports = {
                         }
                     }]
             },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: 
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    }
+            },
         ]
     }
 }
