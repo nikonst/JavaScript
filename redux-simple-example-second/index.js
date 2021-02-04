5cd639d5a46259ca8dc7b6996dd06b6c0dd575df
nikonst <nikonst@otenet.gr>
2021-02-04 13:34:32 +0200

    Add middleware

--- redux-simple-example-second/index.js
+++ redux-simple-example-second/index.js
@@ -1,33 +1,95 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
 
-const INCREASE_COUNTER = "INCREASE_COUNTER"
+const INCREASE_COUNTER_ALPHA = "INCREASE_COUNTER_ALPHA"
+const INCREASE_COUNTER_BETA = "INCREASE_COUNTER_BETA"
+const INCREASE_DAYS_ALPHA = "INCREASE_DAYS_ALPHA"
+const INCREASE_DAYS_BETA = "INCREASE_DAYS_BETA"
 
-function increaseCounter() { // action creator
+function increaseCounterAlpha() { // action creator
     return { // action
-        type: INCREASE_COUNTER,
+        type: INCREASE_COUNTER_ALPHA,
         payload: 1
     }
 }
 
-const initialState = {
-    counter: 0
+function increaseCounterBeta() { // action creator
+    return { // action
+        type: INCREASE_COUNTER_BETA,
+        payload: 1
+    }
 }
 
-const reducer = (state = initialState, action) => {
+function increaseDaysAlpha() { // action creator
+    return { // action
+        type: INCREASE_DAYS_ALPHA,
+        payload: 1
+    }
+}
+
+function increaseDaysBeta() { // action creator
+    return { // action
+        type: INCREASE_DAYS_BETA,
+        payload: 1
+    }
+}
+
+const initialStateCounters = {
+    counterAlpha: 0, 
+    counterBeta: 0
+}
+
+const initialStateDays = {
+    daysAlpha: 0, 
+    daysBeta: 0
+}
+
+const reducerCounters = (state = initialStateCounters, action) => {
     switch(action.type) {
-        case INCREASE_COUNTER: return {
+        case INCREASE_COUNTER_ALPHA: return {
             ...state, // other propertiesof state are unchanged
-            counter: state.counter + 1
+            counterAlpha: state.counterAlpha + 1
+        }
+        case INCREASE_COUNTER_BETA: return {
+            ...state, // other propertiesof state are unchanged
+            counterBeta: state.counterBeta + 1
         }
         default: return state
     }
 }
 
-const store = redux.createStore(reducer) // reducer holds the initial state
-console.log(store)
-console.log(store.getState())
-store.subscribe(()=> console.log("State updated"))
-store.dispatch(increaseCounter())
-store.dispatch(increaseCounter())
-store.dispatch(increaseCounter())
-console.log(store.getState())
+const reducerDays = (state = initialStateDays, action) => {
+    switch(action.type) {
+        case INCREASE_DAYS_ALPHA: return {
+            ...state, // other propertiesof state are unchanged
+            daysAlpha: state.daysAlpha + 1
+        }
+        case INCREASE_DAYS_BETA: return {
+            ...state, // other propertiesof state are unchanged
+            daysAlpha: state.daysAlpha + 1
+        }
+        default: return state
+    }
+}
+
+const logger = reduxLogger.createLogger()
+const applymiddleware = redux.applyMiddleware
+
+const rootReducer = redux.combineReducers({
+    counters: reducerCounters,
+    days: reducerDays
+})
+
+const store = redux.createStore(rootReducer, applymiddleware(logger)) // reducer holds the initial state
+
+console.log("**** ", store)
+console.log("**** ", store.getState())
+//store.subscribe(()=> console.log("State updated"))
+store.dispatch(increaseCounterAlpha())
+store.dispatch(increaseCounterAlpha())
+store.dispatch(increaseCounterBeta())
+store.dispatch(increaseDaysBeta())
+store.dispatch(increaseDaysBeta())
+store.dispatch(increaseDaysAlpha())
+
+console.log("**** ", store.getState())
