55e4c00f896974ded5372083feee5d6a30c5e6a7
nikonst <nikonst@otenet.gr>
2021-02-06 21:40:51 +0200

    Add Redux Dev Tools

--- react-redux-simple-example/src/redux/store.js
+++ react-redux-simple-example/src/redux/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import counterReducer from './counter/counterReducer'
 import hitReducer from './hit/hitReducer'
 
 const rootReducer = combineReducers({counterReducer: counterReducer, hitReducer: hitReducer})
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools())
 
 export default store
