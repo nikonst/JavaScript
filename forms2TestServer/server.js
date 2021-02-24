eab2aaac05054a107773d9f6600aa1da57eb6fb5
nikonst <nikonst@otenet.gr>
2021-02-24 11:00:59 +0200

    Change server cors

--- forms2TestServer/server.js
+++ forms2TestServer/server.js
@@ -2,14 +2,27 @@ const express = require('express')
 const app = express()
 const port = 5050
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get('/student', (req, res) => {
-  res.json({'name':'Hello Student!',
+  res.send(JSON.stringify({'name':'Hello Student!',
     })
+	)
 })
 
 app.get('/teacher', (req, res) => {
-    res.json({'name':'Hello Teacher!',
-        'school':'ΓΥΜΝΑΣΙΟ'})
+	let jsonObj = {'name':'Hello Teacher!',
+        'school':'ΓΥΜΝΑΣΙΟ'}
+	let obj = JSON.parse(jsonObj);
+    res.send(obj)
+    
   })
 
 app.listen(port, () => {
