2875a93469a37e377df16c5850284ac4d1874127
nikonst <nikonst@otenet.gr>
2024-05-27 10:52:57 +0300

    Basic setup

---
+++ graphQLBooksExample/server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+
+const app = express()
+
+const PORT = 4000
+
+app.use('graphql', graphqlHTTP({
+    
+}))
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
