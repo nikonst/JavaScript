e4f7b8adc8e07a03cb9b93bfe80858402cada401
nikonst <nikonst@otenet.gr>
2024-08-27 17:17:56 +0300

    Check tsconfig

--- typescript/startOver/dist/someFunctions.js
+++ typescript/startOver/dist/someFunctions.js
@@ -1,21 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.printPerson = exports.printOrPrameters = exports.printArray = exports.myFun = void 0;
-var myFun = function (x) {
+const myFun = (x) => {
     return x + "!";
 };
 exports.myFun = myFun;
-var printArray = function (ar) {
-    ar.forEach(function (a) {
+const printArray = (ar) => {
+    ar.forEach((a) => {
         console.log(a);
     });
 };
 exports.printArray = printArray;
-var printOrPrameters = function (p) {
+const printOrPrameters = (p) => {
     console.log(p);
 };
 exports.printOrPrameters = printOrPrameters;
-var printPerson = function (p) {
+const printPerson = (p) => {
     console.log(p.name, " ", p.age);
 };
 exports.printPerson = printPerson;
