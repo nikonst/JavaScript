03061dcb43aebdc95ddcf150c058c80bcb2282fd
nikonst <nikonst@otenet.gr>
2024-08-29 11:35:50 +0300

    Try to use bootstrap

--- webpack/exampleTwo/src/index.js
+++ webpack/exampleTwo/src/index.js
@@ -2,6 +2,8 @@ import joke from "./joke"
 import './styles/main.scss'
 import laugh from './imgs/laugh.png'
 import generateJoke from './generateJoke'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 const laughIcon = document.getElementById('laughImg')
 laughIcon.src = laugh
--- webpack/exampleTwo/webpack.config.js
+++ webpack/exampleTwo/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
     mode: 'production',
@@ -23,12 +24,40 @@ module.exports = {
     module: {
         rules: [
             {
+                mimetype: 'image/svg+xml',
+                scheme: 'data',
+                type: 'asset/resource',
+                generator: {
+                filename: 'icons/[hash].svg'
+                }
+                },
+            {
                 test:/\.scss$/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ]
+                    {
+                      // Adds CSS to the DOM by injecting a `<style>` tag
+                      loader: 'style-loader'
+                    },
+                    {
+                      // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                      loader: 'css-loader'
+                    },
+                    {
+                      // Loader for webpack to process CSS with PostCSS
+                      loader: 'postcss-loader',
+                      options: {
+                        postcssOptions: {
+                          plugins: [
+                            autoprefixer
+                          ]
+                        }
+                      }
+                    },
+                    {
+                      // Loads a SASS/SCSS file and compiles it to CSS
+                      loader: 'sass-loader'
+                    }
+                  ],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
