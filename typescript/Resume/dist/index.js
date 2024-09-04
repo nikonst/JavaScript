222cf1ca6fa402b3b9d6902d9469c129f51541e9
nikonst <nikonst@otenet.gr>
2024-09-04 10:27:40 +0300

    Tuples

--- typescript/Resume/dist/index.js
+++ typescript/Resume/dist/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("HELO TSC");
+let nums = [1, 2, 3];
+nums.forEach(n => {
+    console.log(n);
+});
+let user = ["Nikos", 123];
+console.log(user);
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
--- typescript/Resume/src/index.ts
+++ typescript/Resume/src/index.ts
@@ -1,1 +1,16 @@
-console.log("HELO TSC")
+// Arrays
+let nums: number[]  = [1,2,3]
+
+nums.forEach(n => {
+    console.log(n)
+})
+
+//Tuples
+let user: [string, number] = ["Nikos", 123]
+console.log(user)
+
+//Enums
+enum Size {Small = 1, Medium = 2, Large = 3}
+let mySize = Size.Medium
+console.log(mySize)
+
