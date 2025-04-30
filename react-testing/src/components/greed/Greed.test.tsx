963169d8e9463af87124d6ba828170b84dfd9cc9
nikonst <nikonst@otenet.gr>
2025-04-30 08:45:15 +0300

    Grouping tests

--- react-testing/src/components/greed/Greed.test.tsx
+++ react-testing/src/components/greed/Greed.test.tsx
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react"
 import { Greed } from "./Greed"
 
-test('Greed Renders Correctly', () => {
-    render(<Greed/>)
-    const textElement = screen.getByText("Hello")
-    expect(textElement).toBeInTheDocument()
+describe('Greed', () => {
+    test('Renders correctly', () => {
+        render(<Greed/>)
+        const textElement = screen.getByText("Hello")
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    test('Renders with a name', () => {
+        render(<Greed name = 'Aspa'/>)
+        const textElement = screen.getByText("Hello Aspa")
+        expect(textElement).toBeInTheDocument()
+    })
 })
 
-test('Greed renders with a name', () => {
-    render(<Greed name = 'Aspa'/>)
-    const textElement = screen.getByText("Hello Aspa")
-    expect(textElement).toBeInTheDocument()
-})
