0160e9f62095f39db7bca36d01fdea4aad73fa50
nikonst <nikonst@otenet.gr>
2025-04-30 09:11:21 +0300

    Use 'it' instead of 'test'

--- react-testing/src/components/greed/Greed.spec.tsx
+++ react-testing/src/components/greed/Greed.spec.tsx
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react"
 import { Greed } from "./Greed"
 
 describe('Greed', () => {
-    test('Renders correctly', () => {
+    it('Renders correctly', () => {
         render(<Greed/>)
         const textElement = screen.getByText("Hello")
         expect(textElement).toBeInTheDocument()
     })
     
-    test('Renders with a name', () => {
+    it('Renders with a name', () => {
         render(<Greed name = 'Aspa'/>)
         const textElement = screen.getByText("Hello Aspa")
         expect(textElement).toBeInTheDocument()
