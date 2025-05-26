e086ed9b202dad495860233721995bc18a7287a0
nikonst <nikonst@otenet.gr>
2025-05-26 09:15:38 +0300

    Binding Classes

--- Svelte/hello-world/src/App.svelte
+++ Svelte/hello-world/src/App.svelte
@@ -1,14 +1,20 @@
 <script>
 	//export let name;
-	const name = "Hardy"
+	const name = "EveryOne"
 	const someStr = "<b><i>Brand New Day</i></b>"
 	const someAttr = 'myHeading'
 	const disabled = true
+	const status = "danger"
+	const isPromoted = true
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<h3>{5 + 5}</h3>
+	<h2 class="underline">Underlined Text</h2>
+	<h2 class={status}>Status</h2>
+	<h2 class={isPromoted ? "promoted" : "status"}>Promoted</h2>
+	<h2 class:promoted={isPromoted}>Promoted V2</h2>
 	<p>{@html someStr}</p>
 	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
 	<p id = {someAttr}>Using a HTML attribute</p>
@@ -17,6 +23,22 @@
 </main>
 
 <style>
+	.underline {
+		text-decoration: underline;
+	}
+
+	.danger {
+		color: red;
+	}
+
+	.success {
+		color: green;
+	}
+
+	.promoted {
+		font-style: italic;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
