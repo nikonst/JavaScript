4d7ee8cf3b63f3f40ac19e579334ea8f80d1521c
nikonst <nikonst@otenet.gr>
2025-08-12 11:25:20 +0300

    Async fetch

--- reduxToolkit/redux-demo/asyncActions.js
+++ reduxToolkit/redux-demo/asyncActions.js
@@ -1,5 +1,8 @@
 const redux = require("redux")
 const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware
+const thunkMiddleware = require('redux-thunk').default
+const axios = require('axios')
 
 const initialState = {
     loading: false,
@@ -31,7 +34,7 @@ const fetchUsersFailed = (error) => {
     }
 }
 
-const reduxer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case FETCH_USERS_REQUESTED:
             return {
@@ -55,4 +58,24 @@ const reduxer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer)
+const fetchUsers = () => {
+    return function (dispatch) {
+        dispatch(fetchUsersRequested)
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(res => {
+                const users = res.data.map(user => user.id)
+                dispatch(fetchUsersSucceeded, users)
+            })
+            .catch(err => {
+                console.log(err)
+                dispatch(fetchUsersFailed, err)
+            })
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+
+store.subscribe(() => {
+    console.log(store.state)
+})
+store.dispatch(fetchUsers)
