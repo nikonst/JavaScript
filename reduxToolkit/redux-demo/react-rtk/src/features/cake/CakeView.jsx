2e8be7cb78395aa6b34bc584cd566dc87d1f5e8e
nikonst <nikonst@otenet.gr>
2025-08-26 09:57:35 +0300

    Add basic Views

---
+++ reduxToolkit/redux-demo/react-rtk/src/features/cake/CakeView.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const CakeView = () => {
+    return (
+        <div>
+            <h3>CakeView</h3>
+            <p>Number of Caked</p>
+            <p>
+                <button>Order</button>
+                <button>Restock</button>
+            </p>
+        </div>
+    )
+}
---
+++ reduxToolkit/redux-demo/react-rtk/src/features/icecream/IceCreamView.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const IceCreamView = () => {
+    return (
+        <div>
+            <h3>IceCream View</h3>
+            <p>Number of IceCreams</p>
+            <p>
+                <button>Order</button>
+                <button>Restock</button>
+            </p>
+        </div>
+    )
+}
